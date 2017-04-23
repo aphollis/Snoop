24ee6f8250c2df82d58dbfc656af23ae754dccb7
Adam Hollis <nerfdorito@gmail.com>
2017-04-22 21:23:23 -0700

    backend script placeholders

---
+++ backend/dosomething.py
@@ -0,0 +1,17 @@
+"""Various functions for executing terminal commands via browser interface
+"""
+
+def on():
+    pass
+
+def off():
+    pass
+
+def bypass():
+    pass
+
+def status():
+    pass
+
+def alerts():
+    pass

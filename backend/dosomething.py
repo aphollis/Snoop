593ef807c2f456b587db4f204efcc084c9c45025
Adam Hollis <nerfdorito@gmail.com>
2017-05-08 21:52:10 -0700

    fixed 'push' subprocess command

--- backend/dosomething.py
+++ backend/dosomething.py
@@ -1,16 +1,15 @@
 """Various functions for executing terminal commands via browser interface
 """
+import subprocess
 
-def on():
-    pass
-
-def off():
-    pass
+"""el button starts, stops, and outputs status of the vpn server
+   actions are start, stop, status.
+   locale is the server locale, and should probably come from a VPN web source
+"""
+def el_button(action, locale)
+    push=subprocess.run(['sudo', 'systemctl', action, 'openvpn@', locale])
 
 def bypass():
-    pass
-
-def status():
     pass
 
 def alerts():

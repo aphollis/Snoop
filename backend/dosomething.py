dddc709495cf18a231cf62a51eb77dfb6ebf03a4
Adam Hollis <nerfdorito@gmail.com>
2017-05-08 22:06:19 -0700

    added todo

--- backend/dosomething.py
+++ backend/dosomething.py
@@ -6,8 +6,22 @@ import subprocess
    actions are start, stop, status.
    locale is the server locale, and should probably come from a VPN web source
 """
-def el_button(action, locale)
+
+"""TODO: 
+Check if process is running in el_button, process status is used to deterimine 'action' var (start/stop/etc.)
+ 
+Change locale based on list of server locations, this will have to read and write to a file to reset the openvpn locale files, unless all server locations can be set up in the openvpn Dir.
+
+Alerts will need to write to a config file to store info.  what else does our config need?
+
+
+"""
+def el_button(locale):
+    action = ''
     push=subprocess.run(['sudo', 'systemctl', action, 'openvpn@', locale])
+
+
+
 
 def bypass():
     pass

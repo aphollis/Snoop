1c40e42b4032e115ef989cd38445543a151dcd07
Matthew Weidner <weidner.matthew@gmail.com>
2017-05-17 13:54:53 -0700

    building out templates, added placeholders for future functions

--- Snoop/snoop.py
+++ Snoop/snoop.py
@@ -6,14 +6,36 @@ Created on Sat Apr 22 17:07:37 2017
 """
 
 from flask import Flask
-from flask import render_templates
+from flask import render_template
+from flask import request
+import datetime
 
 app = Flask(__name__)
+datetime=str(datetime.datetime.now())
 
 @app.route("/")
 def home():
-   return render_template("home.html")
+   return render_template("home.html",
+                          status="REPLACE WITH STATUS AS STRING",
+                          datetime=datetime)
+
+
+def get_vpncity():
+    query = request.args.get("vpncity")
+    try:
+        available.index(query)
+    except ValueError as v:
+        raise InputError(query)
+
+@app.route("/error")
+def InputError(value):
+       return render_template("error.html", value=value, datetime=datetime)
+
+def get_availablevpncity():
+    # TODO: write function that finds available cities from VPN provider
+    # returns as list of text string
+    return available
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-

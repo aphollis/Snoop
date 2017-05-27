06bba6f7387b32c6ae35bb55df887d07e512e07b
Adam Hollis <nerfdorito@gmail.com>
2017-05-26 22:39:17 -0700

    working on server selection method

--- Snoop/dosomething.py
+++ Snoop/dosomething.py
@@ -90,6 +90,9 @@ def server_select(locale):
         if findit in line:
             print('FOUND IT')
             lines[index] = conf_line
+        elif:
+            index == len(lines)
+            break
 
     print(lines)
 

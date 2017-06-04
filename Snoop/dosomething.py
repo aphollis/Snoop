c963ea4300b0d17f5b9b410481d84b705ff02e45
Adam Hollis <nerfdorito@gmail.com>
2017-06-04 12:05:43 -0700

    import sys

--- Snoop/dosomething.py
+++ Snoop/dosomething.py
@@ -23,7 +23,7 @@ def startstop(action):
     #use subprocess lib to start/stop openvpn, wait for command to complete, print status
     whoami = subprocess.check_output('whoami')
     sys.stderr.write('IAM = ' + str(whoami))
-    command = subprocess.Popen(['sudo', 'systemctl', action.lower(), 'openvpn@snoop'])
+    command = subprocess.Popen(['systemctl', action.lower(), 'openvpn@snoop'])
     command.wait()
     #print("New OpenVPN Status: " + str(status()))
 
